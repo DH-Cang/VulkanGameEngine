6f291a09104e5ea972c80a086a8c70d34b6a39df
DH-Cang <cdh573885@outlook.com>
2024-02-15 16:04:16 +0800

    compile and import shaders

---
+++ compile_shader.bat
@@ -0,0 +1,3 @@
+D:\VulkanSDK\1.3.275.0\Bin\glslc.exe shaders\simple_shader.vert -o shaders\simple_shader.vert.spv
+D:\VulkanSDK\1.3.275.0\Bin\glslc.exe shaders\simple_shader.frag -o shaders\simple_shader.frag.spv
+pause
