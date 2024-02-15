2bb5dacff72fa8b6ff22bfc61004a2b1d9e8700e
DH-Cang <cdh573885@outlook.com>
2024-02-15 14:04:59 +0800

    init

---
+++ xmake.lua
@@ -0,0 +1,15 @@
+set_project("vulkan")
+
+set_arch("x64")
+set_warnings("all")
+set_languages("c++20")
+
+add_rules("mode.debug","mode.releasedbg", "mode.release", "mode.minsizerel")
+
+add_requires("vulkansdk", "glfw", "glm")
+
+target("main")
+    set_default(true)
+    set_kind("binary")
+    add_files("src/main.cpp")
+    add_packages("vulkansdk", "glfw", "glm")
