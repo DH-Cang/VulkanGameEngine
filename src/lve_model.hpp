b216f90da8282a740f8e649f958babdd79fc6450
DH-Cang <cdh573885@outlook.com>
2024-02-26 16:37:59 +0800

    tutorial 17: load obj from files

--- src/lve_model.hpp
+++ src/lve_model.hpp
@@ -9,6 +9,7 @@
 
 // std
 #include <vector>
+#include <memory>
 
 namespace lve
 {
@@ -18,23 +19,34 @@ namespace lve
 
         struct Vertex
         {
-            glm::vec3 position;
-            glm::vec3 color;
+            glm::vec3 position{};
+            glm::vec3 color{};
+            glm::vec3 normal{};
+            glm::vec2 uv{};
 
             static std::vector<VkVertexInputBindingDescription> getBindingDescription();
             static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+
+            bool operator==(const Vertex& other) const
+            {
+                return position == other.position && color == other.color && normal == other.normal && uv == other.uv;
+            }
         };
 
         struct Builder
         {
             std::vector<Vertex> vertices{};
             std::vector<uint32_t> indices{};
+
+            void loadModel(const std::string& filePath);
         };
 
         LveModel(LveDevice& device, const LveModel::Builder& builder);
         ~LveModel();
         LveModel(const LveModel&) = delete;
         LveModel& operator=(const LveModel&) = delete;
+
+        static std::unique_ptr<LveModel> createModelFromFile(LveDevice& device, const std::string& filePath);
 
         void bind(VkCommandBuffer commandBuffer);
         void draw(VkCommandBuffer commandBuffer);
